b373f21fbd75272507b9baa9bc865a2af3e4426e
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-11 19:46:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      low_temperatures << src[row_index].sort[0]
+      low_temperatures << src[row_index][element_index].sort[0]
       element_index += 1 
     end
     row_index += 1 
